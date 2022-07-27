f26bbd7dfa3f239766131d36bfd412c15f7ed402
[Lindsey Dukles] <Lindseydukles@gmail.com>
2022-07-26 22:24:51 -0400

    about and index added

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>
+                About Me
+            </title>
+            <h1>Greetings, I'm Lindsey</h1>
+            <p>This site is to talk about the books I am reading</p>
+        </main>
+
+    )
+}
+
+export default AboutPage
